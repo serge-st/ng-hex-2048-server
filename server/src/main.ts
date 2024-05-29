28c94c555d27dbcf454d5c0adc03aa9371ed2d10
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-05-29 13:36:06 +0300

    feat(server): implement cors and helmet

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@src/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+
+// TODO: move into configuration function
+const origin = 'http://localhost:4200';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin,
+  });
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   await app.listen(3000);
 }
