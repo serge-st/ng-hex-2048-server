3b864c7832b2e97f9a92df8fe014b7d9cf04e283
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-05-31 20:05:56 +0300

    refactor(server/constants): cast as deeply readonly

--- server/src/hex-grid/common/constants/constants.ts
+++ server/src/hex-grid/common/constants/constants.ts
@@ -1,3 +1,3 @@
-export const NEW_HEX_COUNT_INITIAL = 3;
-export const GAME_DIFFICULTY_THRESHOLD = 80;
-export const HEX_HIGH_VALUE_PROBABILITY = 50;
+export const NEW_HEX_COUNT_INITIAL = 3 as const;
+export const GAME_DIFFICULTY_THRESHOLD = 80 as const;
+export const HEX_HIGH_VALUE_PROBABILITY = 50 as const;
