a0f48c7ff0348288f78fd2a5db6a7015baee8f69
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-06-08 15:19:54 +0300

    feat(client/GameControlComponent): improve barely working moving functionality

--- client/src/app/game-control/game-control.component.ts
+++ client/src/app/game-control/game-control.component.ts
@@ -59,6 +59,10 @@ export class GameControlComponent implements OnInit {
           { q: -1, r: -1, s: 2, value: 3 },
           { q: 1, r: -1, s: 0, value: 3 },
           { q: 0, r: 1, s: -1, value: 1 },
+          // test
+          // { q: -2, r: -1, s: 3, value: 6 },
+          // { q: -3, r: 1, s: 2, value: 5 },
+          // { q: -2, r: 1, s: 1, value: 4 },
         ],
         'GameControlComponent.setTextNextTurnHexData()',
       );
@@ -69,28 +73,28 @@ export class GameControlComponent implements OnInit {
     this.setTextNextTurnHexData();
     // this.setNextTurnHexData();
     this.renderer.listen('document', 'keydown', (event) => {
-      switch (event.key.toLowerCase()) {
-        case 'q': {
+      switch (event.code) {
+        case 'KeyQ': {
           this.movePlusS();
           break;
         }
-        case 'w': {
+        case 'KeyW': {
           this.moveMinusR();
           break;
         }
-        case 'e': {
+        case 'KeyE': {
           this.movePlusQ();
           break;
         }
-        case 'a': {
+        case 'KeyA': {
           this.moveMinusQ();
           break;
         }
-        case 's': {
+        case 'KeyS': {
           this.movePlusR();
           break;
         }
-        case 'd': {
+        case 'KeyD': {
           this.moveMinusS();
           break;
         }
@@ -98,9 +102,8 @@ export class GameControlComponent implements OnInit {
     });
   }
 
-  // TODO: have 2 variants with merge and without merge
-  processMove(direction: Direction) {
-    const newHexData = this.hexData.reduce<HexData[]>((acc, hex, i, initialArray) => {
+  processMove(direction: Direction, hexDataArray: HexData[], shouldMerge = false): HexData[] {
+    return hexDataArray.reduce<HexData[]>((acc, hex, i, initialArray) => {
       console.log(`>>>>> INITIATING MOVE FOR HEX: ${hex.value}`);
 
       const comparisonArray = acc.concat(initialArray.slice(i));
@@ -108,92 +111,139 @@ export class GameControlComponent implements OnInit {
       let move = true;
 
       while (move) {
-        const neighborCoords = this.neighbor(hex, direction);
-        const neighborTest = comparisonArray.find((hexData) => isHexAEqualHexB(hexData, neighborCoords));
+        const neighbor = this.getNeighborHex(hex, comparisonArray, direction);
+        const potentialNewHexValue = this.neighbor(hex, direction);
 
-        const requiredHexDataKeys: RequiredHexDataKey[] = ['q', 's', 'r'];
+        const isInRange = this.isHexInRange(potentialNewHexValue);
 
-        const isInRangeTest = !requiredHexDataKeys.some((key) => Math.abs(neighborCoords[key]) > this.radius);
+        console.log(`!!!>>> isInRange: ${isInRange}`);
 
-        console.log(`!!!>>> isInRangeTest: ${isInRangeTest}`);
-
-        if (!isInRangeTest) {
+        if (!isInRange) {
           move = false;
           console.log(`>>>>> is not in range NOT MOVING!`);
           break;
         }
 
-        console.log(`!!!>>> isEmptyTest: ${!neighborTest}`);
+        console.log(`!!!>>> isEmptyTest: ${!neighbor}`);
 
         // isNotEmpty
-        if (neighborTest !== undefined) {
-          const isSameValue = hex.value === neighborTest.value;
+        if (neighbor !== undefined) {
+          const isSameValue = hex.value === neighbor.value;
 
           console.log(`!!!>>> isSameValue: ${isSameValue}`);
 
-          if (!isSameValue) {
+          if (!isSameValue || !shouldMerge) {
             move = false;
             console.log(`>>>>> is not empty NOT MOVING!`);
             break;
           }
 
-          neighborCoords.value! *= 2;
+          potentialNewHexValue.value! *= 2;
           console.log(`!!!>>> merging!`);
-          console.log(`!!!>>> neighborTest: ${JSON.stringify(neighborTest)}`);
+          console.log(`!!!>>> neighbor: ${JSON.stringify(neighbor)}`);
           console.log(`!!!>>> acc: ${JSON.stringify(acc)}`);
-          // todo remove neighborTest from acc
-          acc = acc.filter((hexData) => !isHexAEqualHexB(hexData, neighborTest));
+          acc = acc.filter((hexData) => !isHexAEqualHexB(hexData, neighbor));
         }
         console.log(`!!!>>> moving!`);
-        hex = neighborCoords;
+        hex = potentialNewHexValue;
       }
 
       console.log(`!!!>>> pushing hex: ${JSON.stringify(hex)}`);
       acc.push(hex);
       return acc;
     }, []);
+  }
 
-    // todo: extract saving into movePlus/minus methods
-    this.hexManagementService.setHexData(newHexData, 'GameControlComponent.processMove()');
-    // TODO: implement fetching hexData after move animation complete
+  isHexInRange(hex: HexData): boolean {
+    const requiredHexDataKeys: RequiredHexDataKey[] = ['q', 's', 'r'];
+    const hasViolatedRange = requiredHexDataKeys.some((key) => Math.abs(hex[key]) > this.radius);
+
+    return !hasViolatedRange;
+  }
+
+  getNeighborHex(hex: HexData, comparisonArray: HexData[], direction: Direction): HexData | undefined {
+    const neighborCoords = this.neighbor(hex, direction);
+    return comparisonArray.find((hexData) => {
+      // console.log(`BBB comparing hex from array: ${JSON.stringify(hexData)}`);
+      // console.log(`BBB with neighborCoords: ${JSON.stringify(neighborCoords)}`);
+      // console.log(`BBB isHexAEqualHexB: ${isHexAEqualHexB(hexData, neighborCoords)}`);
+      return isHexAEqualHexB(hexData, neighborCoords);
+    });
+  }
+
+  canMove(hexDataArray: HexData[], direction: Direction): boolean {
+    return hexDataArray.some((hex) => {
+      console.log(`CCC IMPORTANT >>> potentialNextHex: ${JSON.stringify(hex)}`);
+      console.log(`AAA >>> isHexInRange: ${this.isHexInRange(hex)}`);
+      console.log(`AAA >>> comparisonArray: ${JSON.stringify(hexDataArray)}`);
+      console.log(`AAA >>> neighbor hex: ${JSON.stringify(this.getNeighborHex(hex, hexDataArray, direction))}`);
+      console.log(`AAA >>> neighbor hex is empty: ${!this.getNeighborHex(hex, hexDataArray, direction)}`);
+      console.log(
+        `AAA >>> canItMove: ${this.isHexInRange(this.neighbor(hex, direction)) && !this.getNeighborHex(hex, hexDataArray, direction)}`,
+      );
+
+      return this.isHexInRange(this.neighbor(hex, direction)) && !this.getNeighborHex(hex, hexDataArray, direction);
+    });
   }
 
   movePlusS() {
     console.log('Q clicked -> move +s,\n\nr stays the same');
     console.log(`hexData: ${JSON.stringify(this.hexData)},\n\nradius: ${this.radius}`);
-    this.processMove(DIRECTION.PLUS_S);
 
-    // todo: if after first merge there are hexes with empty neighbors
-    // run processMove again without merge till there are no empty neighbors
+    let localHexData = this.hexData;
+
+    let canMove = this.canMove(localHexData, DIRECTION.PLUS_S);
+    let firstLoop = true;
+
+    console.log(`canMove: ${canMove}`);
+
+    let itter = 0;
+
+    while (canMove) {
+      localHexData = this.processMove(DIRECTION.PLUS_S, localHexData, firstLoop);
+      console.log(`AAA localHexData: ${JSON.stringify(localHexData)}`);
+      firstLoop = false;
+      canMove = this.canMove(localHexData, DIRECTION.PLUS_S);
+      console.log(`canMove: ${canMove}`);
+      if (itter > 2) canMove = false;
+      // itter++;
+    }
+
+    this.hexManagementService.setHexData(localHexData, 'GameControlComponent.processMove()');
+
+    // TODO: implement fetching hexData after move animation complete
+
+    // newHexData = this.processMove(DIRECTION.PLUS_S, this.hexData, false);
+    // this.hexManagementService.setHexData(newHexData, 'GameControlComponent.processMove()');
   }
 
   moveMinusR() {
     console.log('W clicked -> move -r,\n\nq stays the same');
 
-    this.processMove(DIRECTION.MINUS_R);
+    // this.processMove(DIRECTION.MINUS_R);
   }
 
   movePlusQ() {
     console.log('E clicked -> move +q,\n\ns stays the same');
 
-    this.processMove(DIRECTION.PLUS_Q);
+    // this.processMove(DIRECTION.PLUS_Q);
   }
 
   moveMinusQ() {
     console.log('A clicked -> move -q,\n\ns stays the same');
 
-    this.processMove(DIRECTION.MINUS_Q);
+    // this.processMove(DIRECTION.MINUS_Q);
   }
 
   movePlusR() {
     console.log('S clicked -> move +r,\n\nq stays the same');
 
-    this.processMove(DIRECTION.PLUS_R);
+    // this.processMove(DIRECTION.PLUS_R);
   }
 
   moveMinusS() {
     console.log('D clicked -> move -s,\n\nr stays the same');
-    this.processMove(DIRECTION.MINUS_S);
+    // this.processMove(DIRECTION.MINUS_S);
   }
 
   addHex(hexA: HexData, hexB: HexData): HexData {
