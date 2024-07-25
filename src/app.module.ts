56a978d1ffd8645c7639191df78980db7b2065ae
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-07-25 18:32:06 +0300

    chore: remove unused and demo code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Logger, Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { HexGridModule, HexGridService } from '@src/hex-grid';
 
 @Module({
   imports: [HexGridModule],
-  controllers: [AppController],
-  providers: [AppService, HexGridService, Logger],
+  controllers: [],
+  providers: [HexGridService, Logger],
 })
 export class AppModule {}
